c21742f916050e2c0708b0bd2af0b5c3188a7a1e
Admin <wiwm07@gmail.com>
2022-01-05 14:38:29 +0800

    light theme

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class DOC extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://font.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
             rel="stylesheet"
           />
         </Head>
