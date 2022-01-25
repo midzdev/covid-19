aef327029c07b0d918854a5988f1d790009921f1
midzdev <admin@midzdev.com>
2022-01-25 10:17:06 +0800

    main

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,7 @@ export default class DOC extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://font.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
-            rel="stylesheet"
-          />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" />
         </Head>
         <Main />
         <NextScript />
