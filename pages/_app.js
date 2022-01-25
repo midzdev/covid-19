e5e01c5a26f036c22bc7bcf6321d07c9d865f901
midzdev <admin@midzdev.com>
2022-01-25 11:52:12 +0800

    new favicon + apple touch icon

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,9 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        <link rel="shortcut icon" href="./image/favicon.ico" />
-        <title>Coronavirus Statistics - TechQuid</title>
+        <link rel="shortcut icon" href="./image/favicon.png" />
+        <link rel="apple-touch-icon" href="./image/apple-touch-icon.png"/ >
+        <title>Coronavirus Statistics</title>
       </Head>
       <Component {...pageProps} />
     </>
