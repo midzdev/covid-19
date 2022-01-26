613bd8ed65381ecb1dee340a58b6ce161ed91eb2
midzdev <admin@midzdev.com>
2022-01-26 18:20:11 +0800

    standalone display for mobile

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,8 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link rel="shortcut icon" href="./image/favicon.png" />
-        <link rel="apple-touch-icon" href="./image/apple-touch-icon.png"/ >
+        <link rel="apple-touch-icon" href="./image/apple-touch-icon.png" />
+        <link rel="manifest" href="./manifest.json" />
         <title>Coronavirus Statistics</title>
       </Head>
       <Component {...pageProps} />
