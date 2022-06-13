95d41f749d5d656a0fe8fcb5135bf27205c4c6bd
MidzDev <wiwm07@gmail.com>
2022-06-13 17:12:37 +0800

    custom babelrc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
-import "../styles/global.css";
-import Head from "next/head";
+import '../styles/global.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="./image/favicon.png" />
         <link rel="apple-touch-icon" href="./image/apple-touch-icon.png" />
         <link rel="manifest" href="./manifest.json" />
-        <title>Coronavirus Statistics</title>
+        <title>Covid-19 - MidzDev</title>
       </Head>
       <Component {...pageProps} />
     </>
