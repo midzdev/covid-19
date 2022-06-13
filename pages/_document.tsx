6d6301a774344924082e4a29dd24035e529c80b8
MidzDev <wiwm07@gmail.com>
2022-06-13 17:34:45 +0800

    _

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
         <link
           href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:wght@600&display=swap"
           rel="stylesheet"
